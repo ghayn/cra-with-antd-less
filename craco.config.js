083ac08aca979409e74bcd226c04b67053719669
ghayn <ghayn@outlook.com>
2019-06-16 15:01:38 +0800

    chore: update dependencies

--- craco.config.js
+++ craco.config.js
@@ -1,19 +1,48 @@
 const CracoLessPlugin = require("craco-less");
 
+const theme = {
+  'primary-color': '#1DA57A',
+}
+
 module.exports = {
   babel: {
     plugins: [
       ["@babel/plugin-proposal-decorators", { "legacy": true, }],
-      ['import', { libraryName: 'antd', style: 'css' }]
+      ['import', { libraryName: 'antd', style: true }],
     ],
   },
-  plugins: [{
-    plugin: CracoLessPlugin,
-    options: {
-      cssLoaderOptions: {
-        modules: true,
-        localIdentName: "[local]_[hash:base64:5]"
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          javascriptEnabled: true,
+          modifyVars: {
+            ...theme,
+          },
+        },
+      },
+    },
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        modifyLessRule: function(lessRule, _context) {
+          lessRule.test = /\.(module)\.(less)$/;
+          lessRule.exclude = /node_modules/;
+
+          return lessRule;
+        },
+        lessLoaderOptions: {
+          javascriptEnabled: true,
+          modifyVars: {
+            ...theme,
+          },
+        },
+        cssLoaderOptions: {
+          modules: true,
+          localIdentName: "[local]_[hash:base64:5]",
+        }
       }
     }
-  }]
+  ]
 };
--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import styles from './App.less';
+import styles from './App.module.less';
 import { Button } from "antd";
+import decoratorTest from './decoratorTest';
 
+@decoratorTest({
+ buttonLabel: 'Learn React Now!'
+})
 class App extends Component {
   render() {
+    const { buttonLabel } = this.props;
+
     return (
       <div className={styles.App }>
         <header className={ styles.AppHeader }>
@@ -12,8 +18,8 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <Button type={"primary"}>
-            Learn React
+          <Button type={"primary"} onClick={ () => window.location.href = 'https://reactjs.org'}>
+            {buttonLabel}
           </Button>
         </header>
       </div>
